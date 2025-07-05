d3506565791095784f8632ef836f5f62d10d3495
alchemistlowkey <alchemistlowkey@gmail.com>
2025-07-05 08:30:29 +0100

    Added navlinks to logout header

--- components/LogoutHeader.jsx
+++ components/LogoutHeader.jsx
@@ -5,6 +5,8 @@ import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { assets } from "@/assets/assets";
 import { useModal } from "./ModelContext";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -13,11 +15,20 @@ const poppins = Poppins({
 
 const LogoutHeader = () => {
   const { openModal } = useModal();
+  const [open, setOpen] = useState(false);
 
   return (
     <header
-      className={`${poppins.className} bg-[linear-gradient(219.84deg,_#474747_4.14%,_#222222_44.22%)] text-white px-5 sm:px-10 md:px-20 flex justify-between items-center`}
+      className={`${poppins.className} bg-[linear-gradient(219.84deg,_#474747_4.14%,_#222222_44.22%)] text-white px-2 sm:px-10 md:px-15 flex justify-between items-center`}
     >
+      {/* =========== Menu button for mobile screen ========== */}
+      <div
+        className="md:hidden"
+        onClick={() => (open ? setOpen(false) : setOpen(true))}
+      >
+        <MenuIcon className="w-6 text-[#E6B027] cursor-pointer" />
+      </div>
+
       <div className="flex items-center">
         <Link href="/">
           <Image
@@ -29,10 +40,33 @@ const LogoutHeader = () => {
           />
         </Link>
       </div>
-      <div className="md:space-x-4 space-x-2">
+
+      {/* +++++++++++++++ Large screen menu links +++++++++++++++++++ */}
+      <div className="space-x-4 md:space-x-6 hidden md:block">
+        <Link
+          href="/"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          Home
+        </Link>
+        <Link
+          href="/properties"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          Properties
+        </Link>
+        <Link
+          href="/about"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          About Us
+        </Link>
+      </div>
+
+      <div className="lg:space-x-4 space-x-2">
         <Button
           onClick={() => openModal("signup")}
-          className={`${poppins.className} py-0 text-xs sm:text-sm md:text-base md:py-2 px-2 md:px-6 bg-[#E6B027] text-white rounded-lg cursor-pointer`}
+          className={`${poppins.className} py-0 text-xs sm:text-sm md:text-base md:py-2 px-1 sm:px-2 lg:px-6 bg-[#E6B027] text-white rounded-lg cursor-pointer`}
         >
           Register
         </Button>
@@ -43,6 +77,33 @@ const LogoutHeader = () => {
         >
           Log In
         </Button>
+      </div>
+
+      {/* ------------------ Mobile menu ------------------ */}
+      <div
+        className={`${
+          open ? "flex" : "hidden"
+        } absolute flex-col sm:top-[90px] top-[50px] bg-[linear-gradient(219.84deg,_#474747_4.14%,_#222222_44.22%)] w-full left-0 sm:px-10 px-2.5 py-2.5 mt-2 sm:mt-0 z-10 pb-10`}
+        onClick={() => setOpen(false)}
+      >
+        <Link
+          href="/"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          Home
+        </Link>
+        <Link
+          href="/properties"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          Properties
+        </Link>
+        <Link
+          href="/about"
+          className="font-medium text-sm md:text-base hover:text-[#E6B027]"
+        >
+          About Us
+        </Link>
       </div>
     </header>
   );
